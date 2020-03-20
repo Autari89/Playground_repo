d792fec62d9a814eea8f0ee34a74ee2ed8ffe21b
Yuri <yuri.g93@gmail.com>
2020-03-20 11:56:56 +0100

    Add a first dictionary API
    Add a first words.com API to test functionalities

--- bot-attempt/send-telegram-message/request_manager.py
+++ bot-attempt/send-telegram-message/request_manager.py
@@ -1,27 +1,41 @@
+import requests
 import json
 import os
 import sys
 
 here = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(here, "./vendored"))
-import requests
 
 telegram_token = os.environ['TELEGRAM_BOT_TOKEN']
+dictionary_token = os.environ['X_RAPID_API_KEY']
+dictionary_host = os.environ['X_RAPID_API_HOST']
 api_url = f"https://api.telegram.org/bot{telegram_token}/"
+dic_api_url = f"https://{dictionary_host}/words/word_token/definitions"
+
+headers = {
+    'x-rapidapi-host': f"{dictionary_host}",
+    'x-rapidapi-key': f"{dictionary_token}"
+}
 
 class Request_manager:
-    def __init__(self, sender_name:str, chat_id:str):
+    def __init__(self, sender_name: str, chat_id: str):
         self.sender_name = sender_name
         self.chat_id = chat_id
 
-    def set_chat_text(self, text:str):
+    def set_chat_text(self, text: str):
         self.chat_text = text
 
     def build_help_answer(self):
         self.final_message = "This is a help message, ask this Bot to translate something from english by writing translate:word_to_translate.\n\nYou can also ask for weather information, just type weather:city_to_check"
 
     def build_translate_answer(self):
-        self.final_message = f'This answer is supposed to be related to translation'
+        word_to_translate = self.chat_text.split(":")
+        new_url = dic_api_url.replace(
+            "word_token", word_to_translate[1].strip())
+        response = requests.request("GET", new_url, headers=headers)
+        response_data = response.json()
+        definition = response_data['definitions'][0]['definition']
+        self.final_message = f'{definition}'
 
     def build_weather_answer(self):
         self.final_message = "This answer is supposed to be related to weather"
@@ -34,7 +48,7 @@ class Request_manager:
         else:
             self.build_help_answer()
 
-    def send_answer(self)->bool:
+    def send_answer(self) -> bool:
         params = {'chat_id': self.chat_id, 'text': self.final_message}
         res = requests.post(api_url + "sendMessage", data=params).json()
         return res
