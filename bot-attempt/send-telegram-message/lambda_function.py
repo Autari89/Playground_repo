30816e577b1cdf6f5bd633fdc9be4a7c7db51c08
Yuri <yuri.g93@gmail.com>
2020-12-22 21:56:28 +0100

    Add word pronunciation to Bot
    - Add possibility to ask for pronunciation of searched word
    - Convert request_manager in global variable in lambda function
      to store status of the bot

--- bot-attempt/send-telegram-message/lambda_function.py
+++ bot-attempt/send-telegram-message/lambda_function.py
@@ -1,13 +1,17 @@
 import json
 from request_manager import Request_manager
 
+request_manager = Request_manager()
+
 def lambda_handler(event, context):
     field_errors = {}    # ignore event parameters other than these 3
     name = event['message']['chat']['first_name']
     text = event['message']['text']
     chat_id = event['message']['chat']['id']
 
-    request_manager = Request_manager(name, chat_id)
+    request_manager.clear_text()
+    request_manager.set_sender_name(name)
+    request_manager.set_chat_id(chat_id)
     request_manager.set_chat_text(text)
     request_manager.process_request()
 
